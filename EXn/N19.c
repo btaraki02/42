6e58463087edf8fdf949c8aaec0d37f10d8c9144
Taraki Bilal <your.email@example.com>
2025-01-06 16:02:06 +0100

    cool

--- EXn/N19.c
+++ EXn/N19.c
@@ -23,6 +23,6 @@ void if_el(char *Alphabet,char *text){
 }
 int main(){
     char Alphabet[]= {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    char text[] = "abx";
+    char text[] = "jjp";
     if_el(Alphabet,text);
 }
